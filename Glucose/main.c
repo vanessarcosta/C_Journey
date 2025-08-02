440028cb8177af50017abdfe9497c9d5c5c1b58e
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-02 20:54:11 +0100

    (Glucose): the user enters the glucose measurement and the program indicates the classification.

---
+++ Glucose/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double glicose;
+
+    printf("Digite a medida da glicose: ");
+    scanf("%lf", &glicose);
+
+    if(glicose <= 100.0)
+    {
+        printf("Classificacao: normal\n");
+    }
+    else if(glicose <= 140.0)
+    {
+        printf("Classificacao: elevado\n");
+    }
+    else
+    {
+        printf("Classificacao: diabetes\n");
+    }
+    return 0;
+}
